3129f61ff10f40149c05cbf7bb73c246e6fdc038
fausto <fausto@aplicacionescorporativas.com>
2023-08-11 07:43:03 -0500

    creando tablas proyecto apitienda

---
+++ database/migrations/2023_08_11_113727_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('last_name', 50);
+            $table->string('address', 50);
+            $table->string('city', 50);
+            $table->unsignedBigInteger('roles_id')->unsigned()->index();
+            $table->foreign('roles_id')->references('id')->on('roles');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
